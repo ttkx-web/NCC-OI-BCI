300ff31e98521cbe24a38200da78b3d3a60a037e
zhangjianyu <2544410390@qq.com>
2026-07-28 17:02:43 +0800

    changed files 0728: 增加了model中的详细调用

--- src/bci_dayloop/models/model_50m/__init__.py
+++ src/bci_dayloop/models/model_50m/__init__.py
@@ -1,1 +1,25 @@
 """50M EEG foundation model integration package."""
+
+from .adapter import (
+    AdapterTiming,
+    Model50MAdapter,
+    RawPredictionResult,
+    build_model50m_adapter,
+)
+from .backbone import Model50MBackbone
+from .classifier import Model50MClassifier
+from .config import Model50MConfig
+from .preprocessing import Model50MPreprocessor
+from .tokenization import Model50MTokenizer
+
+__all__ = [
+    "AdapterTiming",
+    "Model50MAdapter",
+    "Model50MBackbone",
+    "Model50MClassifier",
+    "Model50MConfig",
+    "Model50MPreprocessor",
+    "Model50MTokenizer",
+    "RawPredictionResult",
+    "build_model50m_adapter",
+]
